01f28ffee089d35000621e41fcad50a84e5d8350
pratik(CP) <raipratik788@gmail.com>
2026-04-13 01:40:28 +0530

    send all  the file

--- Designpattern/script.js
+++ Designpattern/script.js
@@ -25,29 +25,113 @@
 // bank.withdraw(11000);
 // bank.checkBalance();
 // yaha khatam hua hai
-function createProduct(name,price){
-let stock=10;
-return {
-    name,
-    price,
-    buy(qty){
-        if(qty<=stock){
-            stock-=qty;
-            console.log(`${qty} picesbooked-${stock} pices left.`);
-        }
-        else{
-            console.error(`We only have ${stock} pices left`);
-        }
-    },
-    refill(qty){
-        stock+=qty;
-        console.log(`refiled the stock-${stock}pices now`);
+// function createProduct(name,price){// factory function pattern
+// let stock=10;
+// return {
+//     name,
+//     price,
+//     buy(qty){
+//         if(qty<=stock){
+//             stock-=qty;
+//             console.log(`${qty} picesbooked-${stock} pices left.`);
+//         }
+//         else{
+//             console.error(`We only have ${stock} pices left`);
+//         }
+//     },
+//     refill(qty){
+//         stock+=qty;
+//         console.log(`refiled the stock-${stock}pices now`);
+//     }
+
+// }
+// }
+// let iphone=createProduct("iphone",7000);
+// iphone.buy(6);
+// let kitkat=createProduct("kitkat",10);
+// kitkat.buy(20); yaha hai khatam hogya hai 
+
+// class  YoutubeChannel{
+//     constructor(){
+//         this.subscribers=[];
+       
+         
+//     }
+//     subscribe(user){
+//         this.subscribers.push(user);
+//          user.update(`${user.name} you have subscribed the channel`);
+//     }
+//     unsubscribe(user){
+//    this.subscribers=this.subscribers.filter((sub)=> sub!==user);
+//     user.update(`you have un-subscibed the youtube channel`);
+//     }
+//     notify(message){
+//         this.subscribers.forEach((sub)=>sub.update(message));
+          
+//     }
+// }
+// class User{
+//     constructor(name){
+//         this.name=name;
+//     }
+//     update(data){
+//         console.log(`${this.name}, ${data}`);
+//     }
+    
+// }
+// let sheryians=new YoutubeChannel();
+// // let user1=new User("Harsh");
+// // let user2=new User("Amit");
+// // sheryians.subscribe(user1);
+// // sheryians.subscribe(user2);
+// // // sheryians.unsubscribe(user1);
+// // sheryians.notify("new vedio uploaded");
+// class YoutubeChannel{
+//     constructor(){
+//         this.subscribers=[];
+
+//     }
+//     subscribe(users){
+//         this.subscribers.push(users);
+//         users.update("You have subcribed the you tube channel");
+//     }
+//     notify(message){
+//         this.subscribers.forEach((sub)=>sub.update(message));
+//     }
+// }
+// class User{
+//     constructor(name){
+//         this.name=name
+//     }
+//     update(message){
+//         console.log(`${this.name},${message}`);
+//     }
+// }
+// let sheriyansh=new YoutubeChannel();
+// let user1=new User("Harsh");
+// sheriyansh.subscribe(user1);
+// sheriyansh.notify("New vedio is uploaded");
+class EmailService{
+    constructor(){
+        this.email=[];
+    }
+    register(users){
+        this.email.push(users);
+        users.update(`you got a new email`);
+    }
+    notify(message){
+        this.email.forEach((em)=>em.update(message));
+    }
+}
+class User{
+    constructor(name){
+        this.name=name;
+    }
+    update(data){
+        console.log(`${this.name},${data}`);
     }
 
 }
-}
-let iphone=createProduct("iphone",7000);
-iphone.buy(6);
-let kitkat=createProduct("kitkat",10);
-kitkat.buy(4);
-<h1>Hello guys</h1>
+let sheriyans=new EmailService();
+let user1=new User("harsh");
+sheriyans.register(user1);
